d20ba93ae67ec364dcdf59dd00f599c535405627
skykim <gk5594@naver.com>
2021-11-30 10:24:37 +0900

    add

---
+++ js/33-40.js
@@ -0,0 +1,18 @@
+//***********************************************************
+//# Date : 2021.11.30
+//***********************************************************
+
+// 문자열 비교하기
+const str1 = 'apple';
+const str2 = 'apple';
+const str3 = 'banana';
+
+document.write('-- ==와 ===의 비교-- <br>');
+document.write(str1 === str2);
+document.write('<br>');
+document.write(str1 == str2);
+document.write('<br>');
+document.write(str2 == str3);
+document.write('<br>');
+document.write(str2 === str3);
+document.write('<br><br>');
