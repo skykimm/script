bd3e085e8cb4305098f3d5a0b67c7396b1d3182a
skykim <gk5594@naver.com>
2021-11-30 12:10:01 +0900

    add3

--- js/33-40.js
+++ js/33-40.js
@@ -81,3 +81,24 @@ for(let i = 0; i < 5; i++){
     }
     document.write('<br>');
 }
+document.write('<br><br>');
+
+//거꾸로 삼각형 별찍기
+for(let i = 0; i < 5; i++){
+    for(let j = 5; j > i; j--){
+        document.write('*');
+    }
+    document.write('<br>');
+}
+document.write('<br><br>');
+
+for(let i = 0; i < 5; i++){
+    for(let j = 0; j <= i; j++){
+        document.write('&nbsp');
+    }
+    for(let j = 5; j > i; j--){
+        document.write('*');
+    }
+    document.write('<br>');
+}
+document.write('<br><br>');
