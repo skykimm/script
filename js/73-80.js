da5642c1e4e5a710a02cc8ba90d32114c9267d3e
skykim <gk5594@naver.com>
2022-01-27 15:00:36 +0900

    73-80

--- js/73-80.js
+++ js/73-80.js
@@ -12,3 +12,71 @@ const json = JSON.stringify(js);
 
 document.write(typeof js + '<br>');
 document.write(json + '<br><br>');
+
+const string = {
+    x : undefined,
+    y : Symbol(''),
+    z : function(){
+        alert('hi')
+    },
+
+    arr : [
+        1,
+        undefined,
+        Symbol(''),
+        function(){
+            alert('hi')
+        }
+    ]
+};
+
+const json2 = JSON.stringify(string);
+document.write(json2 + '<br><br>');
+
+const json3 = `{
+    "id" : 123,
+    "product" :{
+        "type" : "book",
+        "page" : 200,
+        "title" : "sky",
+        "date" : "2022-01-27"
+    }
+}`;
+
+const jsonobj = JSON.parse(json3);
+
+document.write(jsonobj.id + '<br>');
+document.write(jsonobj.product.date + '<br>');
+document.write(jsonobj.product.title + '<br><br>');
+
+function isEmpty(obj){
+    if(obj.constructor === Object
+        && Object.keys(obj).length === 0){
+            return true;
+        }
+        return false;
+}
+const obj1 = {};
+const obj2 = {name : 'js'};
+const str = "javascript";
+
+document.write(isEmpty(obj1) + '<br>');
+document.write(isEmpty(obj2) + '<br>');
+document.write(isEmpty(str) + '<br><br>');
+
+function empty(obj2){
+    if(obj2.constructor !== Object){
+        return false;
+    }
+
+    for(let prop in obj2){
+        if(obj2.hasOwnProperty(prop)){
+            return false;
+        }
+    }
+    return true;
+}
+
+document.write(empty(obj1) + '<br>');
+document.write(empty(obj2) + '<br>');
+document.write(empty(str) + '<br><br>');
